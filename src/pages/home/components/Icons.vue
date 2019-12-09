4340820e327d240cabf09e0b3f726ea5dc8a71d6
vortesnail <1091331061@qq.com>
2019-12-09 22:27:01 +0800

    feat: add swiper and icon ui

---
+++ src/pages/home/components/Icons.vue
@@ -0,0 +1,55 @@
+<template>
+  <div class="icons">
+    <div class="icon">
+      <div class="icon-img">
+        <img class="icon-img-content" src="@/assets/icons/bable.png" alt="icon">
+      </div>
+      <p class="icon-desc">热门书籍</p>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HomeIcons'
+}
+</script>
+
+<style lang="stylus" scoped>
+  @import '~styles/varibles.styl'
+  .icons
+    overflow: hidden
+    width: 100%
+    height: 0
+    padding-bottom: 50%
+    .icon
+      position: relative
+      overflow: hidden
+      float: left
+      width: 25%
+      height: 0
+      padding-bottom: 25%
+      .icon-img
+        position: absolute
+        top: 0
+        left: 0
+        right: 0
+        bottom: .44rem
+        box-sizing: border-box
+        padding: .1rem
+        display: flex
+        align-items: center
+        justify-content: center
+        .icon-img-content
+          display: block
+          height: 80%
+      .icon-desc
+        position: absolute
+        left: 0
+        right: 0
+        bottom: 0
+        height: .44rem
+        line-height: .44rem
+        color: $darkTextColor
+        text-align: center
+</style>
---
+++ src/pages/home/components/Swiper.vue
@@ -0,0 +1,50 @@
+<template>
+  <div class="wrapper">
+    <swiper :options="swiperOption">
+      <swiper-slide v-for="item of swiperList" :key="item.id">
+        <img class="swiper-img" :src="item.imgUrl" />
+      </swiper-slide>
+      <div class="swiper-pagination"  slot="pagination"></div>
+    </swiper>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'HomeSwiper',
+  data () {
+    return {
+      swiperOption: {
+        pagination: '.swiper-pagination',
+        loop: true,
+        autoplay: 3000
+      },
+      swiperList: [{
+        id: '0001',
+        imgUrl: 'http://img.zcool.cn/community/01f7a95543380c0000019ae9819de6.jpg@2o.jpg'
+      }, {
+        id: '0002',
+        imgUrl: 'http://img.zcool.cn/community/010d4f582ec1bca84a0d304f14e1b9.jpg'
+      }, {
+        id: '0003',
+        imgUrl: 'http://img.zcool.cn/community/01a39258eddb07a8012049ef53b617.jpg@1280w_1l_2o_100sh.jpg'
+      }]
+    }
+  }
+}
+</script>
+
+<style lang="stylus" scoped>
+  .wrapper >>> .swiper-pagination-bullets
+    bottom: 20px
+  .wrapper >>> .swiper-pagination-bullet-active
+    background-color: #fff
+  .wrapper
+    overflow: hidden
+    width: 100%
+    height: 0
+    padding-bottom: 31.25%
+    background-color: #eee
+    .swiper-img
+      width: 100%
+</style>
